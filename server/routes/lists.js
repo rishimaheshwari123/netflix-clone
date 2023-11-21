d4dffec3c70c9e050c18736b37e43f6134161dfa
Rishi <rishimaheshwari040@gmail.com>
2023-11-21 10:37:58 +0530

    First Commit

---
+++ server/routes/lists.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const List = require("../models/List");
+const verify = require("../verifyToken");
+
+//Create
+router.post("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        const newList = new List(req.body);
+        try {
+            const savedList = await newList.save();
+            res.status(201).json(savedList);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+//delete
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+
+        try {
+            await List.findByIdAndDelete(req.params.id);
+            res.status(201).json("List has been Deleted successfully");
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+//get 
+router.get("/", verify, async (req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
+    let list = [];
+    try {
+        if (typeQuery) {
+            if (genreQuery) {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery, genre: genreQuery } },
+
+                ])
+            }
+            else {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery } },
+
+                ])
+            }
+
+        } else {
+            list = await List.aggregate([{ $sample: { size: 10 } }]);
+        }
+        res.status(200).json(list);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+
+module.exports = router;
